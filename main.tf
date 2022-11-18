298c754b5bcb8ad54a8a81aa79fde09f71c05e36
mcianci2020 <marcos.cianci@gmail.com>
2022-11-18 19:38:28 -0300

    Update module ec2 in project ...

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 # NTTDATA - DIGITAL ARCHITECTURE
 # Create: Marcos Cianci 
 
-
 ### AWS EC2 - Bastion Host ###
 module "ec2_bastion_host" {
 
     source                      = "git::https://github.com/Digital-Architecture/terraform-modules-aws-ec2.git"
 
     ec2_name                    = "bastion-host-eks-${terraform.workspace}"
-    iam_instance_profile        = data.aws_ami.ubuntu
+    iam_instance_profile        = ""
     ami                         = data.aws_ami.ubuntu.id
+    instance_type               = "t3.small"
     monitoring                  = true
-    vpc_security_group_id       = [""]
-    subnet_id                   = data.aws_subnet.subnet-public-lab-1.id
+    vpc_security_group_ids      = ["module.sg_bastion_host.security_group_id"]
+    subnet_id                   = data.aws_subnet.subnet-public-lab-1.id 
     associate_public_ip_address = true
 
-    user_data                   = data.tamplate_file.user_data_bastionhost.rendered
+    user_data                   = data.template_file.user_data_bastionhost.rendered
 
     root_block_device = [
         {
@@ -29,7 +29,6 @@ module "ec2_bastion_host" {
     ]
 
     tags                    = var.tags
-
 }
 
 
@@ -37,3 +36,34 @@ module "ec2_bastion_host" {
 
 
 ### AWS Security Group ###
+module "sg_bastion_host" {
+
+    source = "git::https://github.com/Digital-Architecture/terraform-modules-aws-security-group.git"
+
+    name_security_group = "scg-ec2-bastion-host-${terraform.workspace}"
+    vpc_id              = data.aws_vpc.vpc-lab-eks.id
+    tags                = var.tags
+
+    sg_rules = [
+        {
+            type        = "ingress"
+            from_port   = 22
+            to_port     = 22
+            protocol    = "tcp"
+            cidr_blocks  = "0.0.0.0/0"
+            description = "Allow SSH"
+        },
+        {
+            type        = "egress"
+            from_port   = 0
+            to_port     = 0
+            protocol    = "-1"
+            cidr_blocks  = "0.0.0.0/0"
+            description = "Allow Traffic Outbound"
+        }
+    ]
+}
+
+
+
+
