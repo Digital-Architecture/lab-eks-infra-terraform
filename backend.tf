a7d7ce56a60c3bf178d719c734403d0545460df1
mcianci2020 <marcos.cianci@gmail.com>
2022-11-22 21:26:13 -0300

    Create eks cluster ....

--- backend.tf
+++ backend.tf
@@ -2,6 +2,38 @@
 # Create: Marcos Cianci 
 
 terraform {
+
+  required_version = ">=0.14"
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+    
+     random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.4.0"
+    }
+
+     cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.12.1"
+    }
+
+  }
+  
   backend "s3" {
       bucket    = "terraform-labs-digital-architecture"
       key       = "terraform/lab-eks-infra.tfstate"
